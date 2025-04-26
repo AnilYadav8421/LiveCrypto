26d6b0747a08e74759ce0fb7e675d1ce4e1faea8
Anil Yadav <anilyadav8149838058@gmail.com>
2025-04-26 12:24:12 +0530

    React-Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Trending from './pages/Trending'
+import Saved from './pages/Saved'
+import Crypto from './pages/Crypto'
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <div>App</div>
+    <div>
+      <Home />
+
+      <Routes location={location}>
+        <Route path="/" element={<Crypto />} />
+        {/* <Route path="/crypto" element={<Crypto />} /> */}
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </div>
+
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { extend } from 'lodash'
 
 
 // https://vite.dev/config/
